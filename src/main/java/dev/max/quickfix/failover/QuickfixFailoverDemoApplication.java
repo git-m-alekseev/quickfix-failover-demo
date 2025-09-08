26619bc539ac5a4f22d02f8eddb33170087e623b
malekseev <git.m.alekseev@gmail.com>
2025-09-08 14:38:58 +0400

    init

---
+++ src/main/java/dev/max/quickfix/failover/QuickfixFailoverDemoApplication.java
@@ -0,0 +1,13 @@
+package dev.max.quickfix.failover;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickfixFailoverDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuickfixFailoverDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/max/quickfix/failover/QuickfixFailoverDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.max.quickfix.failover;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickfixFailoverDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
